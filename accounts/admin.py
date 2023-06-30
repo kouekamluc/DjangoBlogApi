1b282c202774b3ddaeb06ed08ad3a834b03af407
kouekam <luckevinkouekam@gmail.com>
2023-06-30 12:42:32 +0100

    blog app setup

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+
+from .forms import CustomUserChangeForm , CustomUserCreateForm
+from .models import CustomUser
+
 
 # Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreateForm
+    form  = CustomUserChangeForm
+    model = CustomUser
+    
+    list_display = ["email", "username","name","is_staff",]
+    
+
+admin.site.register(CustomUser , CustomUserAdmin)
