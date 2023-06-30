1e080db1caa75d61e609be8414f0e5277e3a0007
kouekam <luckevinkouekam@gmail.com>
2023-06-30 13:22:18 +0100

    add user authentication

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,11 +37,19 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.sites",
+
     
     #3rd party packages
     "rest_framework",
     "corsheaders",
+    "rest_framework.authtoken",
     
+    "allauth",
+    "allauth.account",
+    "allauth.socialaccount",
+    "dj_rest_auth",
+    "dj_rest_auth.registration",
     
     
     #local apps
@@ -52,6 +60,10 @@ INSTALLED_APPS = [
 REST_FRAMEWORK ={
     "DEFAULT_PERMISSION_CLASSES":[
         "rest_framework.permissions.IsAuthenticated",
+    ],
+    "DEFAULT_AUTHENTICATION_CLASSES":[
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
     ]
 }
 
@@ -67,7 +79,7 @@ MIDDLEWARE = [
 ]
 CORS_ORIGIN_WHITELIST=(
     "http://localhost:3000",
-    "http://localhost:5000",
+    "http://localhost:8000",
     
 )
 
@@ -91,10 +103,14 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "django.template.context_processors.request",
             ],
         },
     },
 ]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+SITE_ID = 1
 
 WSGI_APPLICATION = "django_project.wsgi.application"
 
